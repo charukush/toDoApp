af710b8c451cb1a3cf81077baad343d31a102dd1
Charu Kush <charu.kush@gmail.com>
2021-09-14 13:15:13 -0400

    create user saved in AWS

--- user.js
+++ user.js
@@ -1,17 +1,23 @@
 'use strict';
-
+var AWS = require('aws-sdk');
+var docClient = new AWS.DynamoDB.DocumentClient();
 module.exports.createUser = async (event) => {
     var body = event.body;
     var parseBody = JSON.parse(body);
-    var useList =[];
+    var param = {
+        TableName: "todo-user",
+        Item : parseBody
+    };
+    var resp = await docClient.put(param).promise();
+    /*var useList =[];
     useList.push(parseBody);
-    console.log(`User List ${JSON.stringify(useList)}`);
+    console.log(`User added ${JSON.stringify(param)}`);*/
 
     return {
       statusCode: 200,
       body: JSON.stringify(
         {
-          message: 'New user!',
+          message: 'New user added!',
           
         },
 
